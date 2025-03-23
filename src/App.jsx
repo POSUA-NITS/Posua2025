20d590b625c001f18ae8498328843d68a2e6254f
Pragya Prabir Ranjan Deka <www.pprd@gmail.com>
2025-03-24 02:46:48 +0530

    ADD:routes, restructured directory

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,28 @@
-import { useState } from 'react'
+import { lazy, Suspense } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+const Home = lazy(() =>
+  import("./Pages/index").then((module) => ({ default: module.Home }))
+);
+const AboutUs = lazy(() =>
+  import("./Pages/index").then((module) => ({ default: module.AboutUs}))
+);
+// import Footer from "./Components/Shared/Footer/Footer";
+// import Navbar from "./Components/Shared/Navbar/Navbar";
+const App = () => (
+    <main className="relative">
+      {/* <InitialLoadingForHome /> */}
+      <BrowserRouter>
+          {/* <InitialLoadingForHome /> */}
+          {/* <Suspense fallback={<Loading />}> */}
+            {/* <Navbar /> */}
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/AboutUs" element={<AboutUs />} />
+            </Routes>
+            {/* <Footer /> */}
+          {/* </Suspense> */}
+      </BrowserRouter>
+    </main>
+  );
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      home
-    </>
-  )
-}
-
-export default App
+export default App;
