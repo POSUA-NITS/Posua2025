c98c98166821857b03def1f811bcd9310c96ccab
Pragya Prabir Ranjan Deka <129600566+PragyaPR06@users.noreply.github.com>
2025-03-25 13:37:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
+// https://vite.dev/config/
 export default defineConfig({
+  plugins: [react()],
   server: {
+    allowedHosts: ['posua-25.onrender.com'],
+    port: 5173,
     host: true,
-    allowedHosts: [
-      'posua-25.onrender.com',
-    ],
   },
 });
