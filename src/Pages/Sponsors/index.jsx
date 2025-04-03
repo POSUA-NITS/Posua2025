91beeff50bc899b998f520b14c32f3a850e3961f
A-nirvana <anirban_ug_23@gmail.com>
2025-04-03 16:03:00 +0530

    done

--- src/Pages/Sponsors/index.jsx
+++ src/Pages/Sponsors/index.jsx
@@ -37,29 +37,69 @@ const Sponsors = () => {
                 <img src='https://res.cloudinary.com/dmezugavw/image/upload/v1742785498/phul_jhlqod.webp' alt="" className="w-1/2 absolute z-0 opacity-75 -right-[7%] hidden md:block" />
                 {Sponsor("Tea O'Clock", "https://res.cloudinary.com/dmezugavw/image/upload/v1743613139/Tea_i0btx0.webp")}
                 {Sponsor("London Shakes", "https://res.cloudinary.com/dmezugavw/image/upload/v1743613141/shakes_ibx9p2.webp")}
-                {Sponsor("Cropzo", "https://res.cloudinary.com/dmezugavw/image/upload/v1743613144/crops_yegczj.webp")}
+                <div className="relative hidden z-10 md:w-[18vw] tablet:flex flex-col max-w-[60vw] items-center overflow-visible justify-center aspect-square bg-gradient-to-b from-[#E7BB67] to-[#F4E0B9] rounded-lg">
+                    <div className='flex items-center justify-center w-[90%] at-50%_70% aspect-square bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg'>
+                        <img src="https://res.cloudinary.com/dmezugavw/image/upload/v1743613144/crops_yegczj.webp" alt="" className="w-3/4 h-3/4 rounded-full" />
+                    </div>
+                    <div className='bg-[url(https://res.cloudinary.com/dmezugavw/image/upload/v1742785498/sponsor-name_kqelza.webp)] absolute flex items-center justify-center -bottom-[15%] w-[90%] aspect-[25/9] bg-contain bg-center z-10 truncate'>
+                        <h2 className="text-lg md:text-2xl font-semibold text-center text-ellipsis w-full">Cropzo</h2>
+                    </div>
+                </div>
             </div>
             <div className="grid relative grid-cols-1 font-moglan tablet:grid-cols-3 gap-y-10 mt-28 md:mt-44 mb-28 w-[85%] place-items-center">
                 <div className="relative flex overflow-visible z-10 justify-center tablet:w-[22vw] bg-gradient-to-b from-[#E7BB67] to-[#F4E0B9] rounded-lg">
-                    <div className='flex items-center flex-col justify-center w-[90%] py-6 at-50%_70% bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg'>
-                        <h2 className="text-2xl text-center p-4">Why to Sponsor Heading</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id rem blanditiis dolorem soluta iste odit laboriosam voluptatem ex, officiis obcaecati, reiciendis voluptates! Exercitationem, voluptatum cupiditate? Ut tenetur accusantium earum molestiae debitis dignissimos sint inventore fugit at est. Assumenda qui delectus sit suscipit sapiente aperiam dolorem illo tempora numquam ullam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id rem blanditiis dolorem soluta iste odit laboriosam voluptatem ex, officiis obcaecati, reiciendis voluptates! Exercitationem, voluptatum cupiditate? Ut tenetur accusantium earum molestiae debitis dignissimos sint inventore fugit at est. Assumenda qui delectus sit suscipit sapiente aperiam dolorem illo tempora numquam ullam.</p>
+                    <div className='flex items-center flex-col justify-center w-[90%] py-6 at-50%_70% bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg max-h-[90vh] overflow-y-auto'>
+                        <h2 className="text-2xl text-center p-4">Marketing Opportunities</h2>
+                        <p>National Institute of Technology, Silchar,an institute of National Importance is pleased to announce that the institute is organising POSUA'25, the Rongali Bihu Festival of Assam.The entire team of POSUA 2025 is dedicated to ensure maximum reach of our sponsors through the basic framework of the festival. We will make the best of efforts to promote our sponsoring organisations across a wide audience.The team will be popularizing the generous sponsors among students as well as faculty so that they can avail the services rendered by your organization.</p>
+                        <p>Your presence will be highly appreciated and would greatly help us in making this annual event of the Institute a grand success.The Institute will be grateful for your support and faith in us and would surely extend our gratitude towards your organisation. Hence, we, the entire team of POSUA 2025 cordially invite you for sponsorship in this Annual Cultural Fest.</p>
                     </div>
                 </div>
                 <img src='https://res.cloudinary.com/dmezugavw/image/upload/v1742785499/japi_ij9nt2.webp' alt="" className="w-[150%] top-1/4 absolute z-0 opacity-50 -left-[45%] md:hidden block" />
                 <div className="relative flex overflow-visible z-10 justify-center tablet:w-[22vw] bg-gradient-to-b from-[#E7BB67] to-[#F4E0B9] rounded-lg">
-                    <div className='flex items-center flex-col justify-center w-[90%] py-6 at-50%_70% bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg'>
-                        <h2 className="text-2xl text-center p-4">Why to Sponsor Heading</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id rem blanditiis dolorem soluta iste odit laboriosam voluptatem ex, officiis obcaecati, reiciendis voluptates! Exercitationem, voluptatum cupiditate? Ut tenetur accusantium earum molestiae debitis dignissimos sint inventore fugit at est. Assumenda qui delectus sit suscipit sapiente aperiam dolorem illo tempora numquam ullam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id rem blanditiis dolorem soluta iste odit laboriosam voluptatem ex, officiis obcaecati, reiciendis voluptates! Exercitationem, voluptatum cupiditate? Ut tenetur accusantium earum molestiae debitis dignissimos sint inventore fugit at est. Assumenda qui delectus sit suscipit sapiente aperiam dolorem illo tempora numquam ullam.</p>
+                    <div className='flex items-center flex-col justify-center w-[90%] py-6 at-50%_70% bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg max-h-[90vh] overflow-y-auto'>
+                        <h2 className="text-2xl text-center p-4">REASONS TO GET ASSOCIATED WITH POSUA’25</h2>
+                        <p>As POSUA is the Rongali Bihu festival organised with great pomp and joy ,and Bihu being the state festival of Assam ,the annual event with splendid grandeur captures a large audience ,and hence has vast public outreach.This provides a good opportunity of promotion to our associated partners across a wider audience.<br />The association would cast a brand image of your organisation among the students of the institute.As a good percentage of our students are placed with heightened job opportunities,they could be potential associates of your organisation in future endeavours.</p>
+                        <p>Your organisation would have greater reach as POSUA captures a large community of people from across North-Eastern India, students and faculty of NIT Silchar as well as other premier institutes like NITs, IIMs and other colleges. Hence, a huge promising market comprising of prospective customers is waiting for you.</p>
                     </div>
                 </div>
                 <div className="relative flex overflow-visible z-0 justify-center tablet:w-[22vw] bg-gradient-to-b from-[#E7BB67] to-[#F4E0B9] rounded-lg">
-                    <div className='flex items-center flex-col justify-center w-[90%] py-6 at-50%_70% bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg'>
-                    <h2 className="text-2xl text-center p-4">Why to Sponsor Heading</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id rem blanditiis dolorem soluta iste odit laboriosam voluptatem ex, officiis obcaecati, reiciendis voluptates! Exercitationem, voluptatum cupiditate? Ut tenetur accusantium earum molestiae debitis dignissimos sint inventore fugit at est. Assumenda qui delectus sit suscipit sapiente aperiam dolorem illo tempora numquam ullam.</p>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero id rem blanditiis dolorem soluta iste odit laboriosam voluptatem ex, officiis obcaecati, reiciendis voluptates! Exercitationem, voluptatum cupiditate? Ut tenetur accusantium earum molestiae debitis dignissimos sint inventore fugit at est. Assumenda qui delectus sit suscipit sapiente aperiam dolorem illo tempora numquam ullam.</p>
+                    <div className='flex items-center flex-col w-[90%] py-6 at-50%_70% bg-radial from-[#F1DCB5] to-transparent backdrop-blur-2xl rounded-lg max-h-[90vh] overflow-y-auto'>
+                        <h2 className="text-2xl text-center p-4">SPONSORSHIP ENTITLEMENTS</h2>
+                        <p>
+                            1.There will be banners and posters with mention of our sponsors put up in the
+                            college campus as well as across the city during POSUA’25.
+                            <br />
+                            2.We will be sending brochures,posters and flash presentations with the logos of
+                            our sponsors to several engineering and science colleges across the nation.
+                            <br />
+                            3.Publicity pamphlets or any other material provided by sponsors would also be
+                            popularised in the campus during the fest,
+                            <br />
+                            4. There will be special mention of sponsors on the official website of POSUA.The
+                            official website indeed serve as an integral platform for the entire programme as
+                            all important information regarding the fest are put up there.Since the day of
+                            sponsorship confirmation, the logo and link of official websites of our respective
+                            sponsors will be featured in our website
+                            <br />
+                            5.There will be special arrangements made for the sponsors who would prefer
+                            marketing their products and services by setting up stalls and kiosks.
+                            <br />
+                            6.Advertisements published in leading newspapers and magazines regarding the fest
+                            will also carry the name and logo of our sponsors. Also,during the entire fest ,we
+                            will ensure slots and spaces for our sponsoring organisations in regional print
+                            and audio-visual media.
+                            <br />
+                            7.We will be providing information booklets,folders and stationeries issued by the
+                            sponsors ,having their logo and name to all our guests as well as participants.
+                            <br />
+                            8.POSUA also enlists online events which increases the reach of our sponsors
+                            through the internet ,and hence covers almost every part of the country, resulting
+                            in extensive exposure.
+                            <br />
+                            9. After the fest ends,we will be sending proceedings of POSUA’25 to all the
+                            participating colleges,our sponsors and distinguished guests who graced POSUA’25
+                            with their presence.
+                        </p>
                     </div>
                 </div>
             </div>
@@ -67,4 +107,4 @@ const Sponsors = () => {
     )
 }
 
-export {Sponsors}
+export { Sponsors }
